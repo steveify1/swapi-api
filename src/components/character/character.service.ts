b997a0a31e2ae532fd7bc05b5e14489ca33b066b
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2021-09-27 09:47:45 +0100

    feature(character):allow sorting by sascending and descending order

--- src/components/character/character.service.ts
+++ src/components/character/character.service.ts
@@ -1,6 +1,7 @@
 import { delay, inject, injectable } from 'tsyringe';
 import { ServiceMethodOptions } from '../../shared/ServiceMethodOptions';
 import { SwapiService } from '../../shared/swapiService';
+import { Character } from './character.interface';
 
 @injectable()
 export class CharacterService {
@@ -9,14 +10,14 @@ export class CharacterService {
         private readonly swapiService: SwapiService,
     ) {}
 
-    private filterByGender(characters: any[], gender: string): any[] {
+    private filterByGender(characters: Character[], gender: string): Character[] {
         gender = gender.toLowerCase();
         return characters.filter(character => character.gender === gender);
     }
 
-    private generateHeightMetadata(characters: any[]) {
+    private generateHeightMetadata(characters: Character[]) {
         const totalHeightInCentimeters = characters.reduce((prevValue: number, currentCharacter) => {
-            return prevValue + parseFloat(currentCharacter.height);
+            return prevValue + currentCharacter.height;
         }, 0);
 
         return {
@@ -26,11 +27,69 @@ export class CharacterService {
         }
     }
 
-    private generateMetaData(characters: any[]) {
+    private generateMetaData(characters: Character[]) {
         return {
             count: characters.length,
             totalHeight: this.generateHeightMetadata(characters),
         };
+    }
+
+    /**
+     * Sorts the characters by any key with a primitive value either in
+     * ascending or descending order.
+     * 
+     * @param { Character[] } characters - The character list
+     * @param { string } sortKey - The sorting key.
+     */
+    sort(characters: Character[], sortKey: string): Character[] {
+        const useDescendingOrder = sortKey[0] === '-';
+        
+        if (useDescendingOrder) {
+            sortKey = sortKey.slice(1);
+        }
+
+        return characters.sort((a, b) => {
+            const valueInA = a[sortKey];
+            const valueInB = b[sortKey];
+            let result: number;
+            
+            if (!valueInA) {
+                result = -1;
+            }
+
+            if (!valueInB) {
+                result = 1;
+            }
+
+            if (!result) {
+                if (valueInA > valueInB) {
+                    result = 1;
+                } else if (valueInA < valueInB) {
+                    result = -1;
+                } else {
+                    result = 0;
+                }
+            }
+            
+            return useDescendingOrder ? (result * -1) : result;
+        });
+    }
+
+    /**
+     * Normalizes the character list by:
+     *  - converting the `height` and `mass` properties of each character to a Number
+     * 
+     * @param { Character[] } characters - The character list
+     * @returns 
+     */
+    normalize(characters: Character[]): Character[] {
+        return characters.map(character => {
+            return {
+                ...character,
+                height: Number(character.height),
+                mass: Number(character.mass),
+            };
+        });
     }
 
     /**
@@ -46,7 +105,13 @@ export class CharacterService {
         if (query.gender) {
             characters = this.filterByGender(characters, query.gender);
         }
+
+        characters = this.normalize(characters);
         
+        if (query.sort) {
+            characters = this.sort(characters, query.sort);
+        }
+
         const metadata = this.generateMetaData(characters);
 
         return { metadata, characters };
